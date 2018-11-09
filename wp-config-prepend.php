d20a7c627fb3998c17db283d3e7f2f4f92a6404d
David Strauss <david@davidstrauss.net>
2018-11-09 13:03:24 -0800

    Handle WP URI appends and valueless keys.

--- wp-config-prepend.php
+++ wp-config-prepend.php
@@ -4,9 +4,25 @@
 
 function rebuild_qs( $query_string ) {
 	parse_str( $query_string, $parts );
-	return http_build_query( $parts, null, '&', PHP_QUERY_RFC3986 );
+	$qs = http_build_query( $parts, null, '&', PHP_QUERY_RFC3986 );
+
+	// Remove equal signs for keys without values.
+	$qs = rtrim($qs, '=');
+	$qs = str_replace('=&', '&', $qs);
+
+	return $qs;
 }
 
 if ( php_sapi_name() !== 'cli' ) {
-	$_SERVER['QUERY_STRING'] = rebuild_qs( $_SERVER['QUERY_STRING'] );
+	// WordPress appends this to $_SERVER['REQUEST_URI'], but we'll clean
+	// it up anyway.
+	if ( ! empty( $_SERVER['QUERY_STRING'] ) ) {
+		$_SERVER['QUERY_STRING'] = rebuild_qs( $_SERVER['QUERY_STRING'] );
+	}
+
+	// Recover and rebuild the appended query string.
+	$parts = explode( '?', $_SERVER['REQUEST_URI'], 2 );
+	if ( 2 === count( $parts ) ) {
+		$_SERVER['REQUEST_URI'] = $parts[0] . '?' . rebuild_qs( $parts[1] );
+	}
 }
